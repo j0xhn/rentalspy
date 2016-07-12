ff64b99f5275121705387c643f66db73eb069bfe
John D. Storey <johndangerstorey@gmail.com>
2016-07-12 13:25:41 -0600

    sets up client

---
+++ server.js
@@ -0,0 +1,14 @@
+var s3 = require('s3');
+var secrets = require('./private/secrets');
+
+var client = s3.createClient({
+  maxAsyncS3: 20,     // this is the default
+  s3RetryCount: 3,    // this is the default
+  s3RetryDelay: 1000, // this is the default
+  multipartUploadThreshold: 20971520, // this is the default (20 MB)
+  multipartUploadSize: 15728640, // this is the default (15 MB)
+  s3Options: {
+    accessKeyId: secrets.s3.accessKeyId,
+    secretAccessKey: secrets.s3.secretAccessKey,
+  },
+});

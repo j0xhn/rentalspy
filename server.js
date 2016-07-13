1401da9d41849b07706e20ef92bc8a81dfd88328
John D. Storey <johndangerstorey@gmail.com>
2016-07-13 08:52:06 -0600

    gets test sample json working

--- server.js
+++ server.js
@@ -10,5 +10,28 @@ var client = s3.createClient({
   s3Options: {
     accessKeyId: secrets.s3.accessKeyId,
     secretAccessKey: secrets.s3.secretAccessKey,
+    region: secrets.s3.region
   },
+});
+
+var params = {
+  localFile: "testdata/sample.json",
+
+  s3Params: {
+    Bucket: "listing-snapshots",
+    Key: "test/sample.json",
+    // other options supported by putObject, except Body and ContentLength.
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+  },
+};
+var uploader = client.uploadFile(params);
+uploader.on('error', function(err) {
+  console.error("unable to upload:", err.stack);
+});
+uploader.on('progress', function() {
+  console.log("progress", uploader.progressMd5Amount,
+            uploader.progressAmount, uploader.progressTotal);
+});
+uploader.on('end', function() {
+  console.log("done uploading");
 });
